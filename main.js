b30860b08784e23e81051483c20a6ee071ac5def
DeepGajjar7 <93926192+DeepGajjar7@users.noreply.github.com>
2022-05-25 11:16:27 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function setup(){
+    video=createCapture(VIDEO)
+    video.size(550,550)
+    canvas=createCanvas(550,500)
+    canvas.position(560,150)
+    posenet=ml5.poseNet(video,modelLoaded);
+    posenet.on('pose',gotposes)
+}
+
+function draw() {
+    background("#696969")
+}
+
+function modelLoaded(){
+    console.log("model is loaded")
+}
+
+function gotposes(results){
+    if (results.length>0){
+        console.log(results)
+    }
+}
